f55e97d0eb8643baeb97d9c981573bb89c40c2d2
Odera27 <jonathanchidera31@gmail.com>
2025-11-21 18:50:20 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -40,5 +40,4 @@ def ask():
     })
 
 if __name__ == "__main__":
-    # For local testing
-    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
